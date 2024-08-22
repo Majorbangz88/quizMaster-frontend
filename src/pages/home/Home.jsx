5271565dc6aff4bc702e13302a93de2c1522b792
Majorbangz88 <joelchimaobichukwu@gmail.com>
2024-08-22 13:50:21 +0100

    updated my endpoints

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -24,7 +24,7 @@ function Home() {
     const handleRegSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post('http://localhost:5000/api/auth/register', regFormData);
+            const res = await axios.post('https://quizapp-98wt.onrender.com/api/auth/register', regFormData);
             if (res.status === 200) {
                 localStorage.setItem('username', res.data.username);
                 setSuccessMessage('Registration Successful');
@@ -42,7 +42,7 @@ function Home() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post('http://localhost:5000/api/auth/login', formData);
+            const res = await axios.post('https://quizapp-98wt.onrender.com/api/auth/login', formData);
             if (res.status === 200) {
                 const userDetail = {
                     username: res.data.username,
